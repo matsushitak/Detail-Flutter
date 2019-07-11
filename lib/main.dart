c7e7f2539b9570974ebd37ff46a494059f4e829e
trickring <ad.special008@gmail.com>
2019-07-11 22:24:43 +0900

    create title section

--- lib/main.dart
+++ lib/main.dart
@@ -26,12 +26,42 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Widget titleSection = Container(
+    padding: const EdgeInsets.all(32),
+    child: Row(
+      children: <Widget>[
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: Text(
+                  "Oeschinen Lake Campground",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+              Text(
+                "Kandersted Switzerland",
+                style: TextStyle(color: Colors.grey[500]),
+              )
+            ],
+          ),
+        ),
+        Icon(Icons.star, color: Colors.red[500]),
+        Text("41")
+      ],
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text(widget.title),
         ),
-        body: Center());
+        body: Column(
+          children: <Widget>[titleSection],
+        ));
   }
 }
